f065c99d621caa0902bf04ffb0f1a4f0d8d41045
Richard Schwab <mail@w.tf-w.tf>
2014-03-18 11:22:06 +0100

    Add part_detach.py

---
+++ part_detach.py
@@ -0,0 +1,14 @@
+import znc
+
+class part_detach(znc.Module):
+    module_types = [znc.CModInfo.NetworkModule]
+    description = "Client parting is converted in detaching the channel"
+
+    def OnUserPart(self, channel, message):
+        chan = self.GetNetwork().FindChan(channel.s)
+        
+        if not chan.IsDetached() and chan.InConfig():
+            chan.DetachUser()
+            return znc.HALTCORE
+
+        return znc.CONTINUE
